3421de452aba61db04fd74601a2e6d184889334d
AngelinhoSardinha <angel.sardinha.7e8@itb.cat>
2025-11-12 12:56:37 +0100

    d

--- web/index.js
+++ web/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import publicacionesRouter from './routes/publicaciones.js';
 import arquitectosRouter from './routes/arquitectos.js';
 import reformasRouter from "./routes/reformas.js";
+import premiosRouter from "./routes/premios.js";
+import nomenclaturaRouter from "./routes/nomenclatura.js";
+import tipologiaRouter from "./routes/tipologia.js";
+import proteccionRouter from "./routes/proteccion.js";
+import construccionesRouter from "./routes/construcciones.js";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -14,12 +19,20 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.use('/publicaciones', publicacionesRouter);
-app.use("/arquitectos", arquitectosRouter);
-app.use("/reformas", reformasRouter);
+
 app.use("/login", (req, res) => {
   res.render("login");
 });
+
+
+app.use('/publicaciones', publicacionesRouter);
+app.use("/arquitectos", arquitectosRouter);
+app.use("/reformas", reformasRouter);
+app.use("/premios", premiosRouter);
+app.use("/construcciones", construccionesRouter);
+app.use("/nomenclatura", nomenclaturaRouter);
+app.use("/tipologia", tipologiaRouter);
+app.use("/proteccion", proteccionRouter);
 
 // Start
 app.listen(PORT, () => {
